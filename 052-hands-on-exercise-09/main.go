8c6de78a38d37b992395a452cd226149af5f6930
NGkaltzidis <nikos.gkaltzidis@monitoreal.com>
2023-05-30 23:31:01 +0300

    exercises

--- 052-hands-on-exercise-09/main.go
+++ 052-hands-on-exercise-09/main.go
@@ -2,11 +2,15 @@ package main
 
 import "fmt"
 
-var name = "Nikos"
+var x = 40
 
-const surname = "Gkaltzidis"
+const y = 41
 
 func main() {
-	combined := name + " " + surname
-	fmt.Println(combined)
+	z := 42
+
+	fmt.Printf("The value of x is %v and the type of x is %T\n", x, x)
+	fmt.Printf("The value of y is %v and the type of y is %T\n", y, y)
+	fmt.Printf("The value of z is %v and the type of z is %T\n", z, z)
+
 }
