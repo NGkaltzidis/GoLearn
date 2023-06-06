e737edc70496f85cd6ae076b6c7dd27d48f71729
NGkaltzidis <nikos.gkaltzidis@monitoreal.com>
2023-06-06 14:14:57 +0300

    exercises

--- 058-hands-on-exercise-19/main.go
+++ 058-hands-on-exercise-19/main.go
@@ -15,7 +15,8 @@ func combined() string {
 	s2 := puppy.Barks()
 	s3 := puppy.BigBark()
 	s4 := puppy.BigBarks()
+	puppy.From13()
 
-	s5 := s1 + "\n" + s2 + "\n" + s3 + "\n" + s4
-	return s5
+	s6 := s1 + "\n" + s2 + "\n" + s3 + "\n" + s4
+	return s6
 }
---
+++ 059-hands-on-exercise-20/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/NGkaltzidis/puppy"
+
+func main() {
+
+}
+
+func fromPuppy() {
+	puppy.From13()
+}
