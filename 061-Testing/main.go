e96d7211e934f060fd7d0fe14558bf22e72c53b0
NGkaltzidis <nikos.gkaltzidis@monitoreal.com>
2023-06-07 18:29:36 +0300

    if_statements

--- 061-Testing/main.go
+++ 061-Testing/main.go
@@ -2,7 +2,8 @@ package main
 
 import "fmt"
 
-// The init function runs before main, if used
+// The init function runs before main, if used. In every other case / scenario, main function is the very first
+// component that runs on the program.
 func init() {
 	fmt.Println("Test1")
 }
---
+++ 065-if-statements/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 40
+	y := 5
+	fmt.Printf("x = %v \ny = %v\n", x, y)
+
+	if x < 42 {
+		fmt.Println("Less than meaning of life")
+	}
+
+	if x < 42 {
+		fmt.Println("Less than meaning of life")
+	} else {
+		fmt.Println("Equal to, or greater than, the meaning of life")
+	}
+	if x < 42 {
+		fmt.Println("Less than meaning of life")
+	} else if x == 42 {
+		fmt.Println("Equal to the meaning of life")
+	} else {
+		fmt.Println("Greater than the meaning of life")
+	}
+}
