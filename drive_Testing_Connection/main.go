fca32026e8a75e7e22c1b9dfc74bc8277a7b26ab
NGkaltzidis <nikos.gkaltzidis@monitoreal.com>
2023-06-21 15:40:52 +0300

    continue

---
+++ drive_Testing_Connection/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	device := "/dev/disk3"
+	cmd := exec.Command("diskutil", "info", device)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		if strings.Contains(string(output), "could not find disk") {
+			fmt.Println(device, "is not connected.")
+		} else {
+			if err.Error() == "exit status 1" {
+				fmt.Println(device, "is not connected.")
+			}
+			fmt.Println("Error executing command:", err.Error())
+		}
+		return
+	}
+
+	fmt.Println(device, "is connected.")
+}
