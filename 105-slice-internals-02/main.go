f55e4e39852eb8e6abae9d77d2aef11113b70058
NGkaltzidis <nikos.gkaltzidis@monitoreal.com>
2023-07-04 15:09:50 +0300

    further_slices

--- 105-slice-internals-02/main.go
+++ 105-slice-internals-02/main.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+func main() {
+	// In this case we copy the values, instead of referencing b from a, SO ->
+	// When changing values to "a" slice
+	// slice b remains untouched
+
+	a := []int{0, 1, 2, 3, 4, 5}
+	// b := a
+	b := make([]int, 6)
+	copy(b, a)
+
+	fmt.Println("a", a)
+	fmt.Println("b", b)
+	fmt.Println("-------")
+
+	a[0] = 7
+	fmt.Println("a", a)
+	fmt.Println("b", b)
+	fmt.Println("-------")
+}
